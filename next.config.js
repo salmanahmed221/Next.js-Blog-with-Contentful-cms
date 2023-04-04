21ec7c81721f277d2a06266d49b9baf6c6bf33cb
unknown <salmanahmed121221@gmail.com>
2023-04-04 11:36:57 +0500

    Next.js Blog with Contentful cms

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  env: {
+    CONTENTFUL_SPACE_ID: "sjbfac0etnzq",
+    CONTENTFUL_ACCESS_KEY: "jCx2YXHFqg8CYvMGQWOUjuMST9oDqoAQCyJ3iYIO2mA"
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        port: '',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,43 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Image from 'next/image';
 
-const inter = Inter({ subsets: ['latin'] })
+async function getBlogs() {
+  const res = await fetch(
+    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/entries?access_token=${process.env.CONTENTFUL_ACCESS_KEY}&content_type=blogs`,
+    { cache: 'no-store' }
+  );
+  if (!res.ok) {
+    throw new Error('Failed to Fetch');
+  }
+  return res.json();
+}
 
-export default function Home() {
+export default async function Page() {
+  const blogs = await getBlogs();
+  console.log(blogs);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="bg-gray-600 grid md:grid-cols-4 py-4 gap-y-4">
+      {blogs.items.map((elem1: any) => (
+        <div
+          className="bg-white p-5 mx-5"
+          key={elem1.sys.id}>
+          <h1 className="text-2xl font-bold">{elem1.fields.blogTitle}</h1>
+          <p className=" text-justify pt-1">{elem1.fields.description}</p>
+
+          {blogs.includes.Asset.map((elem2: any) => (
+            <div key={elem2.sys.id}>
+              {elem1.fields.image.sys.id == elem2.sys.id ?
+                <Image
+                  src={'https:' + elem2.fields.file.url}
+                  alt=""
+                  width={400}
+                  height={400}
+                  className="mt-2"
+                /> : <div></div>}
+            </div>
+          ))}
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      ))}
+    </div>
+  );
 }
